44066da2628b93bbef8810ff3028795c26d3a900
AnzinFelipe <ian.felipe.drunen@gmail.com>
2025-05-05 16:08:56 -0300

    Mensagem

---
+++ Método da Bisseção.py
@@ -0,0 +1,29 @@
+print("Utilize o método da bisseção para achar a raiz aproximada da função 2x² + 6x - 10")
+
+erro = 10**-4
+a = float(input("\nDigite o primeiro ponto: "))
+b = float(input("Digite o segundo ponto: "))
+
+f_a = 2 * a**2 + 6 * a - 10
+f_b = 2 * b**2 + 6 * b - 10
+
+while True:
+    if f_a > 0 and f_b > 0 or f_a < 0 and f_b < 0:
+        print("Não existe raiz no intervalo selecionado")
+        break
+    
+    media = (a + b) / 2
+    f_a = 2 * a**2 + 6 * a - 10
+    f_b = 2 * b**2 + 6 * b - 10
+    f_media = 2 * media**2 + 6 * media - 10
+
+    if f_media * f_a < 0:
+        b = media
+    elif f_media * f_b < 0:
+        a = media
+
+    if b - a <= erro:
+        break    
+
+print(f"\n {a} | {b} | {f_a} | {f_b} | {media} | {f_media} \n")
+print(f"A raiz aproximada fica no ponto {media:.3f}\n")
---
+++ Método do Trapézio.py
@@ -0,0 +1,18 @@
+def trapezio(n, base, x1):
+    area = 0
+    for i in range(n):
+        partes = ((((2 * (x1 + (i * base))) + ((x1 + (i * base)) ** 2)) + ((2 * (x1 + ((i + 1) * base))) + ((x1 + ((i + 1) * base)) ** 2))) * base) / 2
+        area += partes
+    return area
+
+print("Utilize o método do trapézio para achar a área aproximada da função 2x + x² no intervalo escolhido")
+
+x1 = int(input("Qual o primeiro valor do intervalo? "))
+x2 = int(input("E o último? "))
+n = int(input("Quantos trapézios você quer utilizar? "))
+
+base = (x2 - x1) / n
+
+area = trapezio(n, base, x1)
+
+print(f"A área aproximada da função no intervalo de {x1} até {x2} é {area:.2f}")
